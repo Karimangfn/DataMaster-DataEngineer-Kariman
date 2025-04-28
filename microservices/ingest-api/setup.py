4a8358bcb1527cd9ddb5264f5c1ae8bb1cc14f89
Kariman <karimangfn@gmail.com>
2025-04-27 23:07:07 -0300

    criacao do helm chart e pre-commit

---
+++ microservices/ingest-api/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+setup(
+    name="Ingest API",
+    version="0.0.1",
+    author="Kariman",
+    description="Microsserviço para ingestão de dados de uma API",
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
