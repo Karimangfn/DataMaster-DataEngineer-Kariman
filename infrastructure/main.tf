3c64551386c9efd7993d859e69fd1642cd4d192a
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-05-31 19:13:48 -0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -50,7 +50,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   sku_tier = "Free"
-
   role_based_access_control_enabled = true
   local_account_disabled            = false
 }
@@ -65,6 +64,7 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
 }
 
 resource "azurerm_databricks_workspace" "dbw" {
