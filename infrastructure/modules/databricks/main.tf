988c60dd8eb4411f6d7ca8e638a7b8e576e1fd07
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-21 18:21:32 -0300

    Update main.tf

--- infrastructure/modules/databricks/main.tf
+++ infrastructure/modules/databricks/main.tf
@@ -13,6 +13,11 @@ resource "databricks_group_member" "admin" {
   member_id = databricks_service_principal.sp.id
 }
 
+resource "databricks_directory" "dtmaster_folder" {
+  provider = databricks.this
+  path     = "/Repos/dtMaster"
+}
+
 resource "databricks_repo" "repo" {
   count = var.enable ? 1 : 0
 
@@ -21,4 +26,6 @@ resource "databricks_repo" "repo" {
   url    = var.git_repo_url
   path   = "/Repos/dtMaster/service"
   branch = var.git_repo_branch
+
+  depends_on = [databricks_directory.dtmaster_folder]
 }
