afd7982b7b3f38f86ed09b6534856dcb7784bbac
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-21 17:50:03 -0300

    Update main.tf

--- infrastructure/modules/databricks/main.tf
+++ infrastructure/modules/databricks/main.tf
@@ -7,3 +7,9 @@ resource "databricks_repo" "repo" {
   path   = "/Repos/dtMaster/service"
   branch = var.git_repo_branch
 }
+
+resource "databricks_service_principal" "sp" {
+  application_id = "<clientId-do-SP-no-Azure-AD>"
+  display_name   = "dtMasterSPN"
+  allow_cluster_create = true
+}
