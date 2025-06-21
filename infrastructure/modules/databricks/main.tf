69bf53957985971f327fb10871d76f59a5807d20
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-21 19:10:37 -0300

    Update main.tf

--- infrastructure/modules/databricks/main.tf
+++ infrastructure/modules/databricks/main.tf
@@ -1,18 +1,3 @@
-data "databricks_group" "admins" {
-  display_name = "admins"
-}
-
-resource "databricks_service_principal" "sp" {
-  application_id = var.client_id
-  display_name   = "dtMasterSPN"
-  allow_cluster_create = true
-}
-
-resource "databricks_group_member" "admin" {
-  group_id  = data.databricks_group.admins.id
-  member_id = databricks_service_principal.sp.id
-}
-
 resource "databricks_directory" "folder" {
   provider = databricks.this
   path     = "/Repos/dtMaster"
