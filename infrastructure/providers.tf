9ed2192251312d330828baec6ae87d8a53baadf3
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-21 14:21:30 -0300

    Update providers.tf

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -24,7 +24,6 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  alias             = "workspace"
   azure_client_id   = var.client_id
   azure_client_secret = var.client_secret
   azure_tenant_id   = var.tenant_id
