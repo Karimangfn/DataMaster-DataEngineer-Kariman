9d6f33ccca7a735cce190cfee146ce426ac89cb0
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-21 14:11:12 -0300

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,3 +14,20 @@ variable "subscription_id" {
   type = string
 }
 
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
