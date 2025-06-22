a47e9e1fa38fc2b2e2acd84b34664b5c9a455289
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-06-22 02:40:20 -0300

    Update main.tf

--- infrastructure/modules/databricks/main.tf
+++ infrastructure/modules/databricks/main.tf
@@ -22,6 +22,12 @@ resource "databricks_job" "transform_clean_data_process" {
     }
   }
 
+  git_source {
+        url      = var.git_repo_url
+        provider = "github"
+        branch   = var.git_repo_branch
+  }
+
   task {
     task_key        = "bronze_layer"
     description     = "Bronze data transformation"
@@ -30,12 +36,6 @@ resource "databricks_job" "transform_clean_data_process" {
     notebook_task {
       source        = "GIT"
       notebook_path = "/data-processing/bronze/main.ipynb"
-
-      git_source {
-        url      = var.git_repo_url
-        provider = "github"
-        branch   = var.git_repo_branch
-      }
     }
   }
 
