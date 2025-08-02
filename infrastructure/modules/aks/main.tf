4b043bd3e40a5f0163c0c94552302f7ac9b58918
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:10:36 -0300

    Update main.tf

--- infrastructure/modules/aks/main.tf
+++ infrastructure/modules/aks/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_watcher" "default" {
 }
 
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = var.acr_id
+  scope                = var.container_registry_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
