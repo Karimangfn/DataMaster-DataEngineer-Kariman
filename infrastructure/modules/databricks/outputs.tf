f8f9317fb32f2a75f917a1acedd59bb5733af735
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:34:11 -0300

    Update outputs.tf

--- infrastructure/modules/databricks/outputs.tf
+++ infrastructure/modules/databricks/outputs.tf
@@ -1,4 +1,9 @@
-output "databricks_workspace_url" {
-  description = "URL of the Databricks workspace"
-  value       = azurerm_databricks_workspace.dbw.workspace_url
+output "databricks_job_id" {
+  description = "ID of the Databricks job for data processing"
+  value       = databricks_job.data_process.id
+}
+
+output "databricks_job_name" {
+  description = "Name of the Databricks job for data processing"
+  value       = databricks_job.data_process.name
 }
