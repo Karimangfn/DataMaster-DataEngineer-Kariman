527ab9e826acbcb570126da1755449f2c30429d7
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 19:44:09 -0300

    Create main.tf

---
+++ infrastructure/modules/storage/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_storage_account" "lake" {
+  name                     = "${var.prefix}${var.random_id}lake"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+}
+
+resource "azurerm_storage_container" "raw" {
+  name                  = "raw"
+  storage_account_id    = azurerm_storage_account.lake.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "bronze" {
+  name                  = "bronze"
+  storage_account_id    = azurerm_storage_account.lake.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "silver" {
+  name                  = "silver"
+  storage_account_id    = azurerm_storage_account.lake.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "gold" {
+  name                  = "gold"
+  storage_account_id    = azurerm_storage_account.lake.id
+  container_access_type = "private"
+}
