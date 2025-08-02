0753a01fb8f39ba08a51c3d9d2637bf905a29afc
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 19:43:15 -0300

    Create variables.tf

---
+++ infrastructure/modules/aks/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  type = string
+}
+
+variable "random_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "acr_id" {
+  type = string
+}
