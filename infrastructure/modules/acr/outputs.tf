58d2f404acb5307d10b7885dab4d9063c429a7c8
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:05:09 -0300

    Update outputs.tf

--- infrastructure/modules/acr/outputs.tf
+++ infrastructure/modules/acr/outputs.tf
@@ -2,3 +2,8 @@ output "container_registry_name" {
   description = "Name of the Azure Container Registry"
   value       = azurerm_container_registry.acr.name
 }
+
+output "container_registry_id" {
+  description = "ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
+}
