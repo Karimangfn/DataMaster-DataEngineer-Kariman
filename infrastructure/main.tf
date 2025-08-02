d38698d9a6ec94b9380986734ac8ed3d69256335
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:44:27 -0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,15 +35,18 @@ module "aks" {
 module "databricks" {
   source = "./modules/databricks"
 
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  tenant_id     = var.tenant_id
+  prefix              = var.prefix
+  random_id           = module.resource_group.random_id
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
 
-  git_repo_url     = var.git_repo_url
-  git_repo_branch  = var.git_repo_branch
-  workspace_resource_id = azurerm_databricks_workspace.dbw.id
+  client_id           = var.client_id
+  client_secret       = var.client_secret
+  tenant_id           = var.tenant_id
 
-  enable = var.enable_databricks
+  git_repo_url        = var.git_repo_url
+  git_repo_branch     = var.git_repo_branch
+  enable              = var.enable_databricks
 }
 
 module "storage" {
