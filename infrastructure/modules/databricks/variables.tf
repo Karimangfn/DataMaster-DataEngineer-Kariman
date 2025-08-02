949022c5777cc5e912009070b9fda016b8db7026
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:45:32 -0300

    Update variables.tf

--- infrastructure/modules/databricks/variables.tf
+++ infrastructure/modules/databricks/variables.tf
@@ -1,3 +1,19 @@
+variable "prefix" {
+  type = string
+}
+
+variable "random_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
 variable "client_id" {
   description = "Azure Service Principal Client ID"
   type        = string
