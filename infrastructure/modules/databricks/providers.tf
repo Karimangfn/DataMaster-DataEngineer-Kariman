92fbdb5629d6d1755dfbe900fcedb20d659711ac
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-02 20:59:08 -0300

    Update providers.tf

--- infrastructure/modules/databricks/providers.tf
+++ infrastructure/modules/databricks/providers.tf
@@ -13,5 +13,5 @@ provider "databricks" {
   azure_client_id       = var.client_id
   azure_client_secret   = var.client_secret
   azure_tenant_id       = var.tenant_id
-  azure_workspace_resource_id = var.workspace_resource_id
+  azure_workspace_resource_id = azurerm_databricks_workspace.dbw.id
 }
