a8c4309fd22f5c4e7d525a597583085d11872102
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-03 15:12:19 -0300

    Update variables.tf

--- infrastructure/modules/resource_group/variables.tf
+++ infrastructure/modules/resource_group/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
-  type = string
+  description = "Prefix for all resource names"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "Azure region to deploy resources"
+  type        = string
 }
