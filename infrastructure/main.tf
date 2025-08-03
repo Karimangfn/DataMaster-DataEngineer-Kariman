99f52d55b9f0feef4f8b1ede9589854ca5936f0b
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-03 13:22:08 -0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,16 +34,13 @@ module "aks" {
 
 module "databricks" {
   source = "./modules/databricks"
-
   prefix                = var.prefix
   random_id             = module.resource_group.random_id
   location              = module.resource_group.resource_group_location
   resource_group_name   = module.resource_group.resource_group_name
-
   client_id             = var.client_id
   client_secret         = var.client_secret
   tenant_id             = var.tenant_id
-
   git_repo_url          = var.git_repo_url
   git_repo_branch       = var.git_repo_branch
   enable                = var.enable_databricks
@@ -55,4 +52,5 @@ module "storage" {
   random_id           = module.resource_group.random_id
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
+  client_id           = var.client_id
 }
