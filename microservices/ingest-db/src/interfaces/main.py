763109aa6a8da8c8c2a25b02099fa4130650a243
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-03 14:57:19 -0300

    Update main.py

--- microservices/ingest-db/src/interfaces/main.py
+++ microservices/ingest-db/src/interfaces/main.py
@@ -1,3 +1,6 @@
+import json
+
+from datetime import datetime
 from src.application.services.database_ingestion_service import \
     DatabaseIngestionService
 from src.application.validators.env_vars_validator import validate_env_vars
@@ -6,6 +9,7 @@ from src.domain.exceptions.exceptions import (IngestionError,
                                               UnsupportedDatabaseTypeError)
 from src.infrastructure.config.strategy_registry import (CONNECTION_STRATEGIES,
                                                          INGESTION_STRATEGIES)
+from src.infrastructure.storage.azure_blob_uploader import AzureBlobUploader
 from src.infrastructure.logging.logging_setup import get_logger
 
 logger = get_logger(__name__)
@@ -17,12 +21,20 @@ def main():
     using specified connection and query, then outputs the result.
     """
     try:
-        required_vars = ["DB_TYPE", "DB_CONN_STRING", "DB_QUERY"]
+        required_vars = [
+            "DB_TYPE",
+            "DB_CONN_STRING",
+            "DB_QUERY",
+            "STORAGE_CONTAINER",
+            "STORAGE_FOLDER"
+        ]
         env_vars = validate_env_vars(required_vars)
 
         db_type = env_vars["DB_TYPE"].lower()
         conn_string = env_vars["DB_CONN_STRING"]
         query = env_vars["DB_QUERY"]
+        storage_container = env_vars["STORAGE_CONTAINER"]
+        storage_folder = env_vars["STORAGE_FOLDER"]
 
         logger.info(f"Starting database ingestion for DB_TYPE={db_type}")
 
@@ -39,6 +51,16 @@ def main():
 
         service = DatabaseIngestionService(ingestion_strategy)
         data = service.ingest()
+
+        json_str = json.dumps(data, default=str)
+
+        blob_uploader = AzureBlobUploader()
+        blob_name = f"{storage_folder}/data_{datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')}.json"
+        blob_uploader.upload_json(
+            container_name=storage_container,
+            blob_name=blob_name,
+            json_content=json_str
+        )
 
         logger.info(
             "Database ingestion completed successfully."
