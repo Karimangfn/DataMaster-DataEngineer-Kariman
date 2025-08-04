4b021af74db2c1804fc07701c77e907b50077890
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-04 00:07:00 -0300

    Update main.py

--- data-processing/bronze/src/main.py
+++ data-processing/bronze/src/main.py
@@ -1,7 +1,12 @@
+import logging
+
 from config.settings import DATASET_CONFIG
 from modules.bronze_ingestion import ingest_bronze_customer_data
 from modules.schemas import get_customer_schema
 from modules.utils import detect_format_from_extension
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -10,12 +15,10 @@ def main():
     Detects the format of the dataset, loads the schema, and
     starts the Auto Loader process.
     """
+    logger.info("Início da execução do pipeline Bronze")
+    print("teste2")
     raw_files = dbutils.fs.ls(DATASET_CONFIG["input_path"])
     first_file = raw_files[0].name
     file_format = detect_format_from_extension(first_file)
     schema = get_customer_schema()
     ingest_bronze_customer_data(spark, DATASET_CONFIG, schema, file_format)
-
-
-if __name__ == "__main__":
-    main()
