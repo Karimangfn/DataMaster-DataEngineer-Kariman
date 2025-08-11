7c28cae793838e72a21d4056dbf633e149012ec3
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-11 03:46:00 -0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,7 +48,6 @@ module "databricks" {
   location              = module.resource_group.resource_group_location
   resource_group_name   = module.resource_group.resource_group_name
   storage_account_name  = module.storage.storage_account_name
-  storage_account_key   = var.storage_account_key
   client_id             = var.client_id
   client_secret         = var.client_secret
   tenant_id             = var.tenant_id
