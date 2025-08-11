0462718f093316bf9c0860668bb037b162b0649a
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-11 01:58:17 -0300

    Update main.py

--- data-processing/bronze/src/main.py
+++ data-processing/bronze/src/main.py
@@ -2,38 +2,45 @@ import logging
 import sys
 import os
 
-try:
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-except NameError:
-    base_dir = os.getcwd()
-
-print("base_dir:", base_dir)
-print("listdir base_dir:", os.listdir(base_dir))
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 if base_dir not in sys.path:
     sys.path.insert(0, base_dir)
-
-print("sys.path:", sys.path)
 
 from config.settings import DATASET_CONFIG
 from modules.bronze_ingestion import ingest_bronze_customer_data
 from modules.schemas import get_customer_schema
 from utils.utils import detect_format_from_extension
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def main():
     """
     Executes the Bronze ingestion pipeline for customer data.
-    Detects the format of the dataset, loads the schema, and
-    starts the Auto Loader process.
+    Detects the file format, loads the schema, and starts the Auto Loader.
     """
-    logger.info("Início da execução do pipeline Bronze")
-    print("teste2")
+    logger.info("Starting Bronze pipeline execution")
+
+    logger.info(
+        f"Checking files in input path: {DATASET_CONFIG['input_path']}"
+    )
     raw_files = dbutils.fs.ls(DATASET_CONFIG["input_path"])
+
+    if not raw_files:
+        logger.error(
+            f"No files found in {DATASET_CONFIG['input_path']}"
+        )
+        return
+
     first_file = raw_files[0].name
+    logger.info(f"First file found: {first_file}")
+
     file_format = detect_format_from_extension(first_file)
+    logger.info(f"Detected file format: {file_format}")
+
     schema = get_customer_schema()
+    logger.info("Schema loaded successfully")
+
     ingest_bronze_customer_data(spark, DATASET_CONFIG, schema, file_format)
+    logger.info("Bronze step executed successfully")
