ad19c63684f2827fb8fa06f5f66a320ea291f393
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-11 01:59:58 -0300

    Update main.py

--- data-processing/bronze/src/main.py
+++ data-processing/bronze/src/main.py
@@ -2,7 +2,7 @@ import logging
 import sys
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.getcwd()
 
 if base_dir not in sys.path:
     sys.path.insert(0, base_dir)
