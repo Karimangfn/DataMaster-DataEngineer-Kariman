dedc1b54605ddd4d9ced8092b1b5ebb0d0d0096a
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-11 03:45:46 -0300

    Update variables.tf

--- infrastructure/modules/databricks/variables.tf
+++ infrastructure/modules/databricks/variables.tf
@@ -18,12 +18,6 @@ variable "storage_account_name" {
   description = "Name of the Azure Storage Account for Jobs"
 }
 
-variable "storage_account_key" {
-  description = "Storage Account access key to Databricks secret"
-  type        = string
-  sensitive   = true
-}
-
 variable "location" {
   description = "Azure region to deploy resources"
   type        = string
