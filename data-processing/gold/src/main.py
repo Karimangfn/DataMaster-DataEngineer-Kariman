4306b80e11b42929f865c8f5a09dd85ab975a5d9
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-17 16:03:24 -0300

    Update main.py

--- data-processing/gold/src/main.py
+++ data-processing/gold/src/main.py
@@ -1,5 +1,22 @@
+import logging
+import sys
+import os
+
+base_dir = os.getcwd()
+
+if base_dir not in sys.path:
+    sys.path.insert(0, base_dir)
+
 from config.settings import DATASET_CONFIG
 from modules.gold_transform import transform_gold
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def main():
@@ -8,12 +25,15 @@ def main():
     Reads data from the Silver layer, aggregates purchases,
     and writes the results to the Gold layer as a streaming job.
     """
+    logger.info("Starting Gold pipeline execution")
+    
     silver_path = DATASET_CONFIG["silver_path"]
     gold_path = DATASET_CONFIG["gold_path"]
     checkpoint_path = DATASET_CONFIG["gold_checkpoint_path"]
 
     transform_gold(spark, silver_path, gold_path)
 
+    logging.info("Gold transformation pipeline completed successfully.")
 
 if __name__ == "__main__":
     main()
