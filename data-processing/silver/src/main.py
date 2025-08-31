080551f3fb0e5ae5e1cd961d40370045c0643624
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-08-31 20:11:50 -0300

    Update main.py

--- data-processing/silver/src/main.py
+++ data-processing/silver/src/main.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-logger = logging.getLogger("silver_pipeline")
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
