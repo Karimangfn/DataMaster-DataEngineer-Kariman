f77f2fea79b2feec7a5086474b100d7a2fcc3d5e
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-09-29 03:24:56 -0300

    Update outputs.tf

--- infrastructure/modules/databricks/outputs.tf
+++ infrastructure/modules/databricks/outputs.tf
@@ -6,3 +6,8 @@ output "databricks_workspace_url" {
 output "workspace_resource_id" {
   value = azurerm_databricks_workspace.dbw.id
 }
+
+output "databricks_catalog_name" {
+  description = "Name of the Databricks Unity Catalog for this workspace"
+  value       = local.databricks_catalog_name
+}
