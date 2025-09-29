4bf0c8c126bbe3c6adacb6845f29f21846a049c1
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-09-29 00:22:32 -0300

    Update providers.tf

--- infrastructure/modules/databricks/providers.tf
+++ infrastructure/modules/databricks/providers.tf
@@ -8,10 +8,17 @@ terraform {
 }
 
 provider "databricks" {
-  alias = "this"
+  alias                       = "this"
+  azure_client_id             = var.client_id
+  azure_client_secret         = var.client_secret
+  azure_tenant_id             = var.tenant_id
+  azure_workspace_resource_id = azurerm_databricks_workspace.dbw.id
+}
 
+provider "databricks" {
+  alias                 = "accounts"
+  host                  = "https://accounts.azuredatabricks.net"
   azure_client_id       = var.client_id
   azure_client_secret   = var.client_secret
   azure_tenant_id       = var.tenant_id
-  azure_workspace_resource_id = azurerm_databricks_workspace.dbw.id
 }
