43aeb5f531eaea12d9b84d4df1ccebf39f98d3f3
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-09-29 03:47:42 -0300

    Update settings.py

--- data-processing/silver/src/config/settings.py
+++ data-processing/silver/src/config/settings.py
@@ -10,9 +10,21 @@ parser.add_argument(
     required=True,
     help='Storage Account Name'
 )
+parser.add_argument(
+    '--catalog',
+    required=True,
+    help='Databricks Catalog Name'
+)
+parser.add_argument(
+    '--database',
+    required=True,
+    help='Databricks Database Name'
+)
 args = parser.parse_args()
 
 storage_account = args.storage_account
+catalog = args.catalog
+database = args.database
 
 DATASET_CONFIG = {
     "bronze_path": (
@@ -22,5 +34,11 @@ DATASET_CONFIG = {
     "silver_path": (
         f"abfss://silver@{storage_account}.dfs.core.windows.net/"
         "customer_data/"
+    ),
+    "catalog": (
+        catalog
+    ),
+    "database": (
+        database
     )
 }
