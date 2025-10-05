b07f2b7b89f24246a4f2eed76f8bb80d31f67bb8
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 15:17:26 -0300

    Update main.tf

--- infrastructure/modules/access/main.tf
+++ infrastructure/modules/access/main.tf
@@ -1,13 +1,13 @@
-data "azuread_service_principal" "github_actions_spn" {
-  client_id = var.client_id
-}
-
-data "azuread_group" "data_engineers" {
+resource "azuread_group" "data_engineers" {
   display_name = var.data_engineers_group
+  mail_enabled = false
+  security_enabled = true
 }
 
-data "azuread_group" "data_analysts" {
+resource "azuread_group" "data_analysts" {
   display_name = var.data_analysts_group
+  mail_enabled = false
+  security_enabled = true
 }
 
 resource "azurerm_role_assignment" "raw_access" {
