8e58b0ff9bd7ed1459c50f7f16b014974dc8f69c
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 02:57:34 -0300

    Update outputs.tf

--- infrastructure/modules/databricks/outputs.tf
+++ infrastructure/modules/databricks/outputs.tf
@@ -3,6 +3,11 @@ output "databricks_workspace_url" {
   value       = azurerm_databricks_workspace.dbw.workspace_url
 }
 
+output "databricks_workspace_name" {
+  description = "Databricks workspace name"
+  value       = azurerm_databricks_workspace.dbw.name
+}
+
 output "workspace_resource_id" {
   value = azurerm_databricks_workspace.dbw.id
 }
