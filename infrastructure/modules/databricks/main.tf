d981c40a02d05e2901798a60657469f83a5bdfb5
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 08:44:55 -0300

    Update main.tf

--- infrastructure/modules/databricks/main.tf
+++ infrastructure/modules/databricks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_databricks_workspace" "dbw" {
 }
 
 locals {
-  databricks_catalog_name = replace(azurerm_databricks_workspace.dbw.name, "-", "_")
+  databricks_catalog_name = "data_catalog"
 }
 
 resource "databricks_schema" "data_processing_db" {
