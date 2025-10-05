250023304fc49ab8b13a09c74f153db6cc9efa8a
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 15:24:06 -0300

    Update variables.tf

--- infrastructure/modules/access/variables.tf
+++ infrastructure/modules/access/variables.tf
@@ -18,16 +18,6 @@ variable "gold_container_id" {
   description = "ID of the Gold container in the Storage Account"
 }
 
-variable "data_engineers_group" {
-  type        = string
-  description = "Name of the Data Engineers group in Azure AD"
-}
-
-variable "data_analysts_group" {
-  type        = string
-  description = "Name of the Data Analysts group in Azure AD"
-}
-
 variable "client_id" {
   type        = string
   description = "Client ID of the Service Principal used by GitHub Actions"
