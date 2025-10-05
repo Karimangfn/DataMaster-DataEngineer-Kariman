762c2b6c4a07a636e552b94f4726deba3e749cbc
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 15:28:26 -0300

    Update main.tf

--- infrastructure/modules/access/main.tf
+++ infrastructure/modules/access/main.tf
@@ -11,23 +11,23 @@ resource "azuread_group" "data_analysts" {
 resource "azurerm_role_assignment" "raw_access" {
   scope                = var.raw_container_id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_group.data_engineers.object_id
+  principal_id         = azuread_group.data_engineers.object_id
 }
 
 resource "azurerm_role_assignment" "bronze_access" {
   scope                = var.bronze_container_id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_group.data_engineers.object_id
+  principal_id         = azuread_group.data_engineers.object_id
 }
 
 resource "azurerm_role_assignment" "silver_access" {
   scope                = var.silver_container_id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_group.data_analysts.object_id
+  principal_id         = azuread_group.data_analysts.object_id
 }
 
 resource "azurerm_role_assignment" "gold_access" {
   scope                = var.gold_container_id
   role_definition_name = "Storage Blob Data Reader"
-  principal_id         = data.azuread_group.data_analysts.object_id
+  principal_id         = azuread_group.data_analysts.object_id
 }
