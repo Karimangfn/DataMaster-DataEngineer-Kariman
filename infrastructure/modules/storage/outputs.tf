1d3235a9fd2685e4e5f14571a0f1892b3ec9dbe1
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-05 14:56:35 -0300

    Update outputs.tf

--- infrastructure/modules/storage/outputs.tf
+++ infrastructure/modules/storage/outputs.tf
@@ -2,3 +2,28 @@ output "storage_account_name" {
   description = "Name of the created Storage Account"
   value       = azurerm_storage_account.lake.name
 }
+
+output "storage_account_name" {
+  description = "Name of the created Storage Account"
+  value       = azurerm_storage_account.lake.name
+}
+
+output "raw_container_id" {
+  description = "ID of the 'raw' storage container"
+  value       = azurerm_storage_container.raw.id
+}
+
+output "bronze_container_id" {
+  description = "ID of the 'bronze' storage container"
+  value       = azurerm_storage_container.bronze.id
+}
+
+output "silver_container_id" {
+  description = "ID of the 'silver' storage container"
+  value       = azurerm_storage_container.silver.id
+}
+
+output "gold_container_id" {
+  description = "ID of the 'gold' storage container"
+  value       = azurerm_storage_container.gold.id
+}
