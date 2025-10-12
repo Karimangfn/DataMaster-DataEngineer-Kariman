d61249ef673fe1e49d20a3672151a187245dc995
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-12 10:12:28 -0300

    Update access.py

--- data-processing/bronze/src/utils/access.py
+++ data-processing/bronze/src/utils/access.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 def grant_access_to_bronze(spark: SparkSession, catalog: str, database: str):
     """
-    Grants permissions on the Bronze schema/tables to the data engineers group.
+    Grants permissions on the Bronze schema and catalog.
 
     Args:
         spark (SparkSession): Spark session object.
@@ -13,6 +13,15 @@ def grant_access_to_bronze(spark: SparkSession, catalog: str, database: str):
         database (str): Databricks schema (database) name.
     """
     try:
+        logger.info(f"Granting USE CATALOG permissions on {catalog}...")
+
+        groups = ["data_engineers", "data_scientists", "data_analysts"]
+
+        for group in groups:
+            spark.sql(f"""
+                GRANT USE CATALOG ON CATALOG {catalog} TO `{group}`
+            """)
+        
         bronze_schema = f"{catalog}.{database}.bronze"
 
         logger.info(f"Granting ALL PRIVILEGES permissions on {bronze_schema} to data_engineers group...")
@@ -22,7 +31,6 @@ def grant_access_to_bronze(spark: SparkSession, catalog: str, database: str):
         """)
 
         logger.info(f"Permissions granted successfully on {bronze_schema}")
-
     except Exception as e:
         logger.error(f"Error granting permissions on Bronze layer: {e}")
         raise
