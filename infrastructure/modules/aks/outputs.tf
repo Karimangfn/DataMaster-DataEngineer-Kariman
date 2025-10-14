6fc026c41d606a14720fb0e3c04936db598c4a41
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2025-10-14 04:15:10 -0300

    Update outputs.tf

--- infrastructure/modules/aks/outputs.tf
+++ infrastructure/modules/aks/outputs.tf
@@ -8,3 +8,8 @@ output "kube_config" {
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
 }
+
+output "kubernetes_name" {
+  description = "Name of the Azure Kubernetes Service"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
